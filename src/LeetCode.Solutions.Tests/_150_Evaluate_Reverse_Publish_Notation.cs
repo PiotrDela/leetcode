2966a1bd23a88b91bf1799fcee0a8d60dd079100
Piotr Dela <piotr.dela@gmail.com>
2024-01-18 14:41:02 +0100

    150. Evaluate Reverse Polish Notation

---
+++ src/LeetCode.Solutions.Tests/_150_Evaluate_Reverse_Publish_Notation.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace LeetCode.Solutions.Tests
+{
+    public class _150_Evaluate_Reverse_Publish_Notation_Tests
+    {
+        [Theory]
+        [InlineData(new string[] { "2", "1", "+", "3", "*" }, 9)]
+        [InlineData(new string[] { "4", "13", "5", "/", "+" }, 6)]
+        [InlineData(new string[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" }, 22)]
+        public void Test(string[] tokens, int expectedResult)
+        {
+            Assert.Equal(expectedResult, new _150_Evaluate_Reverse_Publish_Notation().EvalRPN(tokens));
+        }
+    }
+}
---
+++ src/LeetCode.Solutions/_150_Evaluate_Reverse_Publish_Notation.cs
@@ -0,0 +1,46 @@
+﻿//Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+//Implement the MinStack class:
+
+//    MinStack() initializes the stack object.
+//    void push(int val) pushes the element val onto the stack.
+//    void pop() removes the element on the top of the stack.
+//    int top() gets the top element of the stack.
+//    int getMin() retrieves the minimum element in the stack.
+
+//You must implement a solution with O(1) time complexity for each function.
+
+namespace LeetCode.Solutions;
+
+public class _150_Evaluate_Reverse_Publish_Notation
+{
+    private static readonly Dictionary<string, Func<int, int, int>> calculations = new Dictionary<string, Func<int, int, int>>()
+    {
+        {"+", (a, b) => a + b },
+        {"-", (a, b) => a - b },
+        {"*", (a, b) => a * b },
+        {"/", (a, b) => a / b },
+    };
+
+    public int EvalRPN(string[] tokens)
+    {
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            var token = tokens[i];
+            if (int.TryParse(token, out var number))
+            {
+                stack.Push(number);
+                continue;
+            }
+
+            var second = stack.Pop();
+            var first = stack.Pop();
+
+            stack.Push(calculations[token](first, second));
+        }
+
+        return stack.Pop();
+    }
+}
